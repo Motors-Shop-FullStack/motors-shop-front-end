44a0cce13c0152fd8398d28efcf9ec8e038d404a
Danilo <danvalerio89@gmail.com>
2022-10-25 14:39:32 -0300

    fix: app content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { UpdateProfileModal } from "./components/Modal/UpdateProfileModal";
 import GlobalStyles from "./styles/global";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <UpdateProfileModal />
     </>
   );
 }
